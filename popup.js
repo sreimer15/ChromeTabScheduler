28c3173a8bb2ff977270b2e4808c79a7b28f2497
Sean Reimer <sreimer15@gmail.com>
2016-03-11 22:20:53 -0800

    Adds the ability to add more inputs to the interface adding more links

--- popup.js
+++ popup.js
@@ -1,15 +1,54 @@
 $(document).ready(function(){
   console.log('this is within the popup.js')
-  console.log( $('#linksForm').submit );
+  // Defines what Number to append to name of input tag
+  var currentNum = 1;
+
+  $("#addMoreButton").on('click', function(){
+    currentNum++;
+    console.log(currentNum)
+    var linkInput = [ "<input placeholder='Input Link'",
+                       "name=link" + currentNum.toString(),
+                       "</input>"
+                     ]
+  var timeInput = [
+                  '<input placeholder="How many seconds from now?"',
+                  'name=time' + currentNum.toString(),
+                  'type="number"></input>'
+                  ]
+
+  linkInput = linkInput.join(' ')
+  timeInput = timeInput.join(' ')
+
+  $("#addMoreSection").append(linkInput)
+  $("#addMoreSection").append(timeInput)
+     
+  });
+
   $("#linksForm").submit(function(event){
-    console.log('let us see if it gets to linksForm' )
     
-    // Gets all inputs in an array
-    var $inputs = $('#submitLinks :input')
+    var formData = {};
+    var listOfLinks =[];
+    // Let's get formData from all Links including time
+    
+
     var formData = {
-      'url': $('input[name=firstLink]').val()
+      'url': $('input[name=link' + "1" +']').val(),
+      'time': $('input[name=time' + "1" +']').val()
     }
-    console.log(formData, 'This is the formData')
+    
+    var scheduleOpening = function(urlObject){
+      console.log(urlObject);
+      var currentUrl = urlObject.url
+      var relevantTime = urlObject.time
+
+    setTimeout(function(){
+      chrome.tabs.create({ url : currentUrl})  
+    },relevantTime)
+
+    }
+
+   scheduleOpening(formData);
+
     event.preventDefault();
   });
 
